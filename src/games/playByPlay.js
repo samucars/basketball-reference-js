9a5bf69af57210d77858b06a82b18cda6d79e82c
Samuel Rocha <samuca_rs96@hotmail.com>
2019-07-07 22:13:19 -0300

    test: improving test coverage

--- src/games/playByPlay.js
+++ src/games/playByPlay.js
@@ -5,11 +5,8 @@ const Crawler = require('../crawler');
 const quarters = ['q1', 'q2', 'q3', 'q4'];
 
 const normalizeQuarter = quarter => Number(quarter.replace('q', ''));
-const getTeamPositionTable = element => (
-  element.find('td:nth-child(2)').text().trim() !== ''
-    ? 2
-    : 6
-);
+const normalizePlay = play => play.replace(/(\r\n|\n|\r)/gm, '');
+const getTeamPositionTable = element => (element.find('td:nth-child(2)').text().trim() !== '' ? 2 : 6);
 
 module.exports = async (url) => {
   const html = await Crawler.request(url, '.table_outer_container');
@@ -41,7 +38,7 @@ module.exports = async (url) => {
     const play = {
       time: $(element).find('td:nth-child(1)').text().trim(),
       team: teamPositionTable === 2 ? firstTeam : secondTeam,
-      play: $(element).find(`td:nth-child(${teamPositionTable})`).text().trim(),
+      play: normalizePlay($(element).find(`td:nth-child(${teamPositionTable})`).text().trim()),
       scoring: Number($(element)
         .find('.bbr-play-score:nth-child(3)').text().trim()
         .replace('+', '')),
--- test/playByPlay.spec.js
+++ test/playByPlay.spec.js
@@ -14,8 +14,63 @@ describe('A function that contains play-by-play of the game', () => {
   afterEach(() => {
     stubRequest.restore();
   });
-  it('should', async () => {
-    stubRequest.callsFake(() => Promise.resolve(mocks.playByPlay));
-    const playByPlay = await Games.playByPlay('/boxscores/pbp/201812130HOU.html');
+  describe('when execute a request successfully', () => {
+    let playByPlay;
+    beforeEach(async () => {
+      stubRequest.callsFake(() => Promise.resolve(mocks.playByPlay));
+      playByPlay = await Games.playByPlay('/boxscores/pbp/201812130HOU.html');
+    });
+    it('should return the data of the first quarter', () => {
+      const [firstQuarter] = playByPlay;
+      assert.equal(firstQuarter.quarter, 1);
+      assert.equal(firstQuarter.plays[0].time, '11:35.0');
+      assert.equal(firstQuarter.plays[0].team, 'LA Lakers');
+      assert.equal(firstQuarter.plays[0].play, 'L. James makes 2-pt jump shot from 11 ft');
+      assert.equal(firstQuarter.plays[0].scoring, 2);
+
+      assert.equal(firstQuarter.plays[1].time, '11:17.0');
+      assert.equal(firstQuarter.plays[1].team, 'Houston');
+      assert.equal(firstQuarter.plays[1].play, 'C. Paul misses 2-pt jump shot from 16 ft');
+      assert.equal(firstQuarter.plays[1].scoring, 0);
+    });
+    it('should return the data of the second quarter', () => {
+      const secondQuarter = playByPlay[1];
+      assert.equal(secondQuarter.quarter, 2);
+      assert.equal(secondQuarter.plays[0].time, '11:44.0');
+      assert.equal(secondQuarter.plays[0].team, 'Houston');
+      assert.equal(secondQuarter.plays[0].play, 'G. Green misses 3-pt jump shot from 25 ft');
+      assert.equal(secondQuarter.plays[0].scoring, 0);
+
+      assert.equal(secondQuarter.plays[1].time, '11:43.0');
+      assert.equal(secondQuarter.plays[1].team, 'LA Lakers');
+      assert.equal(secondQuarter.plays[1].play, 'Defensive rebound by L. Stephenson');
+      assert.equal(secondQuarter.plays[1].scoring, 0);
+    });
+    it('should return the data of the third quarter', () => {
+      const thirdQuarter = playByPlay[2];
+      assert.equal(thirdQuarter.quarter, 3);
+      assert.equal(thirdQuarter.plays[0].time, '11:43.0');
+      assert.equal(thirdQuarter.plays[0].team, 'Houston');
+      assert.equal(thirdQuarter.plays[0].play, 'J. Harden makes 2-pt layup from 3 ft');
+      assert.equal(thirdQuarter.plays[0].scoring, 0);
+
+      assert.equal(thirdQuarter.plays[1].time, '11:21.0');
+      assert.equal(thirdQuarter.plays[1].team, 'LA Lakers');
+      assert.equal(thirdQuarter.plays[1].play, 'J. Hart makes 3-pt jump shot from 24 ft          (assist by L. James)');
+      assert.equal(thirdQuarter.plays[1].scoring, 3);
+    });
+    it('should return the data of the fourth quarter', () => {
+      const fourthQuarter = playByPlay[3];
+      assert.equal(fourthQuarter.quarter, 4);
+      assert.equal(fourthQuarter.plays[0].time, '11:48.0');
+      assert.equal(fourthQuarter.plays[0].team, 'LA Lakers');
+      assert.equal(fourthQuarter.plays[0].play, 'L. Ball misses 3-pt jump shot from 25 ft');
+      assert.equal(fourthQuarter.plays[0].scoring, 0);
+
+      assert.equal(fourthQuarter.plays[1].time, '11:46.0');
+      assert.equal(fourthQuarter.plays[1].team, 'LA Lakers');
+      assert.equal(fourthQuarter.plays[1].play, 'Offensive rebound by T. Chandler');
+      assert.equal(fourthQuarter.plays[1].scoring, 0);
+    });
   });
 });
